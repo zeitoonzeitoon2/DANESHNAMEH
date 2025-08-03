912b777192f55a86cfb4ae793f4a808ca26175d0
zeitoonzeitoon2 <zeitoonzeitoon2@gmail.com>
2025-08-04 02:37:26 +0330

    Configure base path for Netlify deployment

--- concept-tree/vite.config.js
+++ concept-tree/vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // این خط جدید را اضافه کنید
 })
